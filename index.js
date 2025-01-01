4fd5d0ed76c40546a0352590e9e88903c2b60b6a
Mohd Aman <148184025+mdaman2100@users.noreply.github.com>
2025-01-02 00:40:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+var x=7;
+// pranav = [1,2,3];
+
+
+function getname(){
+    console.log("namaste javascript");
+} 
+
+getname();
+console.log(x);
+// console.log(pranav);    
+// const accountid = 144332
+//  let accemail="aman@123"
+//  var accpswd="12345"
+
+//  accemail ="ansari@123"
+//  accpswd ="6789"
+//  console.table([accemail,accpswd])
+ 
